f88ef9a6408bde49a0a31f6d98b6248d26aaf3ee
shunta <shunta711k2000@outlook.jp>
2022-11-30 14:06:45 +0900

    APIを叩いてデータベースにアクセス

--- api_sample.py
+++ api_sample.py
@@ -1,7 +1,13 @@
 import requests
+import json
 
 url = "http://127.0.0.1:8000/all"
 
 r = requests.get(url)
 
-print(r.text)
+data = r.json()
+
+for v in data.values():
+    for i in range(len(v)):
+        # v[i] : i番目のタプル
+        print(v[i])
