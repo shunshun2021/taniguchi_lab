5065096b110bcb7ef747ee65e12a80850d226feb
yuki-boss <yukoto58@gmail.com>
2022-12-06 15:52:53 +0900

    最新のフロント

--- front.py
+++ front.py
@@ -2,7 +2,6 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import *
-import PySimpleGUI as sg
 import tkinter as tk
 from tkinter import ttk
 import camera
