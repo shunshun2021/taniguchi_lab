e0e2483dc16d5685cc552d3f1918356f622cad38
yuki-boss <yukoto58@gmail.com>
2022-12-07 14:16:56 +0900

    最新のフロント

--- front.py
+++ front.py
@@ -16,28 +16,28 @@ class MainWindow(QWidget):
         self.initUI() # UIの初期化
 
     def initUI(self): # UIの初期化をするメソッド
-        self.resize(1000, 600) # ウィンドウの大きさの設定(横幅, 縦幅)
+        self.resize(800, 500) # ウィンドウの大きさの設定(横幅, 縦幅)
         self.move(-10, 0) # ウィンドウを表示する場所の設定(横, 縦)
         self.setWindowTitle('居眠り防止アプリケーション') # ウィンドウのタイトルの設定
         #self.setWindowIcon(QIcon('xxxx.jpg')) # ウィンドウ右上のアイコンの設定
 
         # ラベル名の設定
         lbl1 = QLabel('<p><font size="40" color="#00ff00">居眠り防止アプリ</font></p>', self)
-        lbl1.move(300, 10)
+        lbl1.move(250, 10)
         
         btn1 = QPushButton('スタート！', self) # ボタンウィジェット作成
-        btn1.move(200, 200) # ボタンの位置設定(ボタンの左上の座標)
+        btn1.move(150, 200) # ボタンの位置設定(ボタンの左上の座標)
         btn1.resize(200,100)
         btn1.setStyleSheet('QPushButton {background-color: lightseagreen}')
     
         btn2 = QPushButton('設定', self) # ボタンウィジェット作成
         btn2.resize(btn2.sizeHint()) # ボタンのサイズの自動設定
-        btn2.move(600, 200) # ボタンの位置設定(ボタンの左上の座標)
+        btn2.move(550, 200) # ボタンの位置設定(ボタンの左上の座標)
         btn2.setStyleSheet('QPushButton {background-color: lightseagreen}')
         
         btn3 = QPushButton('ログ', self) # ボタンウィジェット作成
         btn3.resize(btn3.sizeHint()) # ボタンのサイズの自動設定
-        btn3.move(600, 280) # ボタンの位置設定(ボタンの左上の座標)
+        btn3.move(550, 280) # ボタンの位置設定(ボタンの左上の座標)
         btn3.setStyleSheet('QPushButton {background-color: lightseagreen}')
             
         self.sub=log_window()
